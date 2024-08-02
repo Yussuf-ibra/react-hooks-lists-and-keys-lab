663f2bcfe32b304e7320c405d6ba353aeb71418f
yussuf <yussuf.ibrahim@student.moringaschool.com>
2024-08-02 09:44:38 +0300

    initial commit

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { startTransition } from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
@@ -6,7 +6,17 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">
+        {/* render ProjectItem components here */}
+        {projects.map((project) => (
+          <ProjectItem
+            key={project.id}
+            name={project.name}
+            about={project.about}
+            technologies={project.technologies} // Pass the technologies prop
+          />
+        ))}
+      </div>
     </div>
   );
 }
